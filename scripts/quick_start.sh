af515a28103e6caac46788a4e70fa868248d0edd
luongtt <28631182+luongtt@users.noreply.github.com>
2024-10-09 09:12:14 +0700

    use absolute path for config, ckp file

--- scripts/quick_start.sh
+++ scripts/quick_start.sh
@@ -4,7 +4,9 @@ if [ ! -f assets/infore/hifigan/g_01140000 ]; then
   wget https://huggingface.co/ntt123/viettts_infore_16k/resolve/main/duration_latest_ckpt.pickle -O assets/infore/nat/duration_latest_ckpt.pickle
   wget https://huggingface.co/ntt123/viettts_infore_16k/resolve/main/acoustic_latest_ckpt.pickle -O assets/infore/nat/acoustic_latest_ckpt.pickle
   wget https://huggingface.co/ntt123/viettts_infore_16k/resolve/main/g_01140000 -O assets/infore/hifigan/g_01140000
-  python3 -m vietTTS.hifigan.convert_torch_model_to_haiku --config-file=assets/hifigan/config.json --checkpoint-file=assets/infore/hifigan/g_01140000
+  cf_file=$(realpath assets/hifigan/config.json)
+  ckp_file=$(realpath assets/infore/hifigan/g_01140000)
+  python3 -m vietTTS.hifigan.convert_torch_model_to_haiku --config-file=$cf_file --checkpoint-file=$ckp_file
 fi
 
 echo "Generate audio clip"
